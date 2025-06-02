c95184eae89f3cb6227d889d154f7396b71626a3
Svetlana <bosekom@bk.ru>
2025-06-02 14:43:22 +0300

    Правки: добавление компанента и облегчение app.jsx

--- react-flashcards/src/App.jsx
+++ react-flashcards/src/App.jsx
@@ -1,25 +1,10 @@
-import Tariff from './components/Tariff';
 import './App.css';
+import TariffList from './components/TariffList';
 
 function App() {
-  const tariffs = [
-    { title: 'Безлимитный 300', price: 300, speed: 10 },
-    { title: 'Безлимитный 450', price: 450, speed: 50 },
-    { title: 'Безлимитный 550', price: 550, speed: 100 },
-    { title: 'Безлимитный 1000', price: 1000, speed: 200 }
-  ];
-
   return (
-    <div className="tariff-container">
-      {tariffs.map((tariff) => (
-        <Tariff
-          key={tariff.price}
-          title={tariff.title}
-          price={tariff.price}
-          speed={tariff.speed}
-          isHighlighted={tariff.price === 550}
-        />
-      ))}
+    <div>
+      <TariffList />
     </div>
   );
 }
